b93e82e8fc27f316c6a054e232a40f93bd252d53
Yusuf Mescioğlu <yusuf@mesci.dev>
2024-07-18 00:27:42 +0300

    SS_C_1

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_super_secret_key'
-    SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID') or 'write_your_clien_id_here'
+    SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID') or 'write_your_client_id_here'
     SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET') or 'write_your_client_secret_here'
     SPOTIPY_REDIRECT_URI = 'http://localhost:5001/callback'
     SESSION_COOKIE_NAME = 'spotify-login-session'
